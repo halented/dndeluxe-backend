04256145b96f848bc085a744ce1438ab6e93e5f5
halented <hallie.dunn1@gmail.com>
2019-04-25 14:49:21 -0700

    working through adding jwt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    skip_before_action :authorzed, only: [:create]
 
     def new
         user = User.find_or_create_by(username: params[:username])
@@ -13,7 +14,8 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
         if @user.valid?
-            render json: { user: UserSerializer.new(@user) }, status: :created
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
             render json: { error: 'failed to create user' }, status: :not_acceptable
         end
