ba86a18f44f3960b815f77d180485e2bac22ab7e
halented <hallie.dunn1@gmail.com>
2019-04-30 14:52:14 -0700

    added some attrs to games migration table

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :games
-  resources :characters
-  resources :users
   # resources :notes
-  # resources :games do
-  #   resources :characters
-  # end
-  # resources :users do
-  #   resources :characters
-  #   post ':id/new-character', to: "characters#new"
-  # end
+  # resources :games
+  # resources :characters
+  # resources :users
+  resources :notes
+  resources :games do
+    resources :characters
+  end
+  resources :users do
+    resources :characters
+    post ':id/new-character', to: "characters#new"
+  end
 
   post '/login', to: 'auth#create'
   post '/profile', to: 'users#profile'
--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,12 @@ ActiveRecord::Schema.define(version: 2019_04_23_000846) do
   end
 
   create_table "games", force: :cascade do |t|
-    t.string "GroupName"
+    t.bigint "user_id"
+    t.string "location"
+    t.string "group_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "notes", force: :cascade do |t|
